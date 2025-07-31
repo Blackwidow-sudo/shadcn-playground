ab3aed2a406a04f7db4d7af2bcc014752b4c3479
Blackwidow-sudo <bob_merlin92@web.de>
2025-07-31 12:48:01 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<main class="container mx-auto flex items-center justify-center h-screen">
+	{@render children()}
+</main>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import DatePicker from "$lib/components/DatePicker.svelte";
+</script>
+
+<DatePicker />
